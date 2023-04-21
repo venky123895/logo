d05508da37996750fae0259f1565590471dd7aaf
venky123895 <devathotivenkataramana@gmail.com>
2023-04-21 07:17:59 +0530

    first commit

---
+++ src/Components/Body.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Body = () => {
+  return (
+    <div style={{
+        width:"100vw",
+        height:"100vh"
+    }}>
+        <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg' alt='imagea' style={{
+            width:'100%',
+            height:'100%',
+        }} >
+            
+        </img>
+        
+    </div>
+  )
+}
+
+export default Body
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './nav.css'
+const Navbar = () => {
+  return (
+    <div className='nav'>
+        <h2>LOGO</h2>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Menu</li>
+            <li>Order</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
